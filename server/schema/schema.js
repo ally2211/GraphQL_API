bd856fb0442a94107a4969fc123b7db42558f1bd
ally2211 <amendieta.va@gmail.com>
2024-10-26 20:52:13 -0500

    Resolve function and test query in graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,27 +1,49 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const _ = require('lodash');
+
 
 // TaskType definition
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
     id: { type: GraphQLString },
-    name: { type: GraphQLString },
+    title: { type: GraphQLString},
+    weight: { type: GraphQLInt },
     description: { type: GraphQLString },
   }),
 });
-
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  },
+    {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order.',
+  },
+  {
+    id: '3',
+    title: 'Learn CSS basics',
+    weight: 1,
+    description: 'Create a style.css file and link it in your HTML file. Add basic styling like background color and font styles.',
+  }
+];
 // RootQuery definition
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     task: {
       type: TaskType,
-      args: {
-        id: { type: GraphQLString },
-      },
+      args: { id: { type: GraphQLString } },
       resolve(parent, args) {
-        // Add code here to fetch data by id from your database
-        return;
+        // Find the task by id
+        //return tasks.find(task => task.id === args.id);
+        // Use lodash's find method to locate the task by id
+        return _.find(tasks, { id: args.id });
       },
     },
   },
